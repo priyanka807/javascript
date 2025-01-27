cb25314594752fb7aa1905f83477c1f3bff90a36
priyanka807 <priyankacoder11@gmail.com>
2025-01-27 12:11:21 +0530

    add strength and compitency  based questions

--- fetchandaxios.js
+++ fetchandaxios.js
@@ -1,10 +1,10 @@
-//fetch and axios are used to make http request .they are diffe in functionality and ease of use 
 
+//fetch and axios are used to make http request .they are diffe in functionality and ease of use 
 //fetch and axios  defination :  built in js api for http request  and : popular http  client library for for js 
-//fetch and axios  ease of use : requires more manual handling of response and simpler syntex , automatic json parsing 
+//fetch and axios ease of use : requires more manual handling of response and simpler syntax, automatic json parsing
 // browser support : native and widly supported in browser and requires additional library installation .
 // error handling : only handle error for network issues , check manually http status and automatically handle http request 
-//feature  : rquires need  extra work for timeout and retries and include advanced fetaures like timeout ,interception and  more.
+//feature  :rquires need  extra work for timeout and retries and include advanced fetaures like timeout ,interception and  more.
 
 fetch("https://jsonplaceholde.typicode.com").then((response)=>{
     if(!response.ok){
@@ -18,8 +18,3 @@ fetch("https://jsonplaceholde.typicode.com").then((response)=>{
 
 axios.get("https://jsonplaceholde.typicode.com").then((data)=>console.log(data,'data in axios'))
 .catch((error)=>console.log(error,'error in axios'))
-
-
-
-
-
